521ad6b37daf644f6a2977494167f9492e600890
poojadayal <poojaadayal@gmail.com>
2025-06-17 22:00:23 +0530

    font-family added
    Signed-off-by: poojadayal <poojaadayal@gmail.com>

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
 
      useEffect(() => {
           const fetchUsers = async () => {
-               await new Promise((res) => setTimeout(res, 2000)); // simulate delay
+               await new Promise((res) => setTimeout(res, 2000)); 
                const res = await fetch("http://localhost:5000/users");
                const data = await res.json();
                setUsers(data);
@@ -26,16 +26,17 @@ const Dashboard = () => {
           user.email.toLowerCase().includes(searchQuery.toLowerCase()) ||
           user.role.toLowerCase().includes(searchQuery.toLowerCase())
      );
+     
 
      return (
           <div className="p-4 space-y-4">
                <Cards />
                <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-                    <div className="lg:col-span-2 space-y-4">
+                    <div className="lg:col-span-2 space-y-2">
                          <Analytics />
                          <Users users={filteredUsers} loading={loadingUsers} />
                     </div>
-                    <div className="lg:col-span-1 h-full ">
+                    <div className="lg:col-span-1 h-[20vh] ">
                          <AddUserForm setUsers={setUsers} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
                     </div>
                </div>
